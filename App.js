a6e4507c87ea1ff2a8beb6be8f96f738407def82
Spencer Hurley <spencer@Spencers-MacBook-Air.local>
2018-06-04 00:38:17 -0400

    initial commit

--- App.js
+++ App.js
@@ -99,12 +99,4 @@ const App = createStackNavigator({
 });
 
 export default App;
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+
